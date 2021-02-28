9907c48c0c78d40db52b186ee4b795aa10ba1842
Hooman <hooman_salamat@yahoo.com>
2021-02-28 10:51:20 -0500

    Update app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+// const path = require('path');
+// var mpath = path.parse(__filename);
+// console.log(mpath);
+
+// const os = require ('os');
+// var totalMemory = os.totalmem();
+// var freeMemory = os.freemem();
+// console.log('Total Memory: ' + totalMemory);
+// console.log(`free memory ${freeMemory}`);
+
+// const fs = require('fs');
+// const files = fs.readdirSync('./');
+// console.log(files);
+
+// fs.readdir('$', function(err, files){
+//     if(err) console.log('Error', err);
+//     else console.log('Result: ' + files);
+// });
+
+
+
